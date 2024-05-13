bbb63622c2ddd58d419e1b55830ab86172f8b2fa
Dany Ramirez <dany.ramirez.92@hotmail.com>
2024-05-14 00:14:57 +0200

    Basic header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,20 @@
+import { Link } from "react-router-dom";
+import '/src/Header.css';
+
 const Header = () => {
-  return <div>Header test</div>;
+  return (
+    <header className="header">
+      <h1 className="title">Stilgalleriet</h1> {/* Stilgalleriet */}
+      <ul className="menu">
+        <Link className="link" to="/register">
+          <li>Register</li>
+        </Link>
+        <Link className="link" to="/login">
+          <li>Login</li>
+        </Link>
+      </ul>
+    </header>
+  );
 };
+
 export default Header;
