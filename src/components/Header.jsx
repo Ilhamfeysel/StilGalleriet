a56a4da336324bb27cff6c0e76b7911279079459
Martin Phu <extramart@outlook.com>
2024-05-15 11:01:47 +0200

    Fix authcontext in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,13 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import '/src/Header.css';
-import { AuthContext } from "./context/Authcontext";
-import HomePageButtons from './HomePageButtons'; 
+import "/src/Header.css";
+import { AuthContext } from "../context/Authcontext";
+import HomePageButtons from "./HomePageButtons";
 import { Button } from "react-bootstrap";
 
 const Header = () => {
-
   const { state } = useContext(AuthContext);
-  const isLoggedIn = state.user !==null;
+  const isLoggedIn = state.user !== null;
 
   return (
     <header className="header">
@@ -16,20 +15,28 @@ const Header = () => {
       <ul className="menu">
         {isLoggedIn ? (
           <>
-          <Link className="link" to="/profile">
-            <li>Profil</li>
-          </Link>
-          <Link className="link" to="/favorites">
-            <li>Favorites</li>
-          </Link>
+            <Link className="link" to="/profile">
+              <li>Profil</li>
+            </Link>
+            <Link className="link" to="/favorites">
+              <li>Favorites</li>
+            </Link>
           </>
         ) : (
           <>
-          <HomePageButtons /> 
-          <Button variant="outline-secondary" onClick={() => 
-            window.location.href='/register'}>Register</Button>{' '}
-          <Button variant="outline-secondary" onClick={() => 
-            window.location.href='/login'}>Login</Button>{' '}
+            <HomePageButtons />
+            <Button
+              variant="outline-secondary"
+              onClick={() => (window.location.href = "/register")}
+            >
+              Register
+            </Button>{" "}
+            <Button
+              variant="outline-secondary"
+              onClick={() => (window.location.href = "/login")}
+            >
+              Login
+            </Button>{" "}
           </>
         )}
       </ul>
