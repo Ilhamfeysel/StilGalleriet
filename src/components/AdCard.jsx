12d977b6e347cc4d4689e8e878b83d0418830fb6
Fazilet1820 <senolfazilet1820@gmail.com>
2024-05-16 19:14:12 +0200

    favorite icon added

--- src/components/AdCard.jsx
+++ src/components/AdCard.jsx
@@ -1,14 +1,26 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
 
 export default function AdCard(props) {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-sm">
+        <div className="col-sm mb-5">
           <div className="card">
-            <img src={props.imgUrl} className="card-img-top" alt="..." />
+            <img
+              src={props.imgUrl}
+              className="card-img-top"
+              alt="product image"
+            />
             <ul className="list-group list-group-flush">
-              <li className="list-group-item">{props.price} </li>
+              <li className="list-group-item d-flex justify-content-between">
+                {props.price}
+                <FontAwesomeIcon
+                  icon={faHeart}
+                  style={{ cursor: "pointer" }}
+                />{" "}
+              </li>
               <li className="list-group-item">{props.size}</li>
               <li className="list-group-item">{props.category}</li>
             </ul>
