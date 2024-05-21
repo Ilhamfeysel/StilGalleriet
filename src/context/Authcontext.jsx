90628b19757e8a03a0f6d56570a3323ffa0f6c7e
Helena <helena@klarr.se>
2024-05-21 14:05:42 +0200

    fixed authContext

--- src/context/Authcontext.jsx
+++ src/context/Authcontext.jsx
@@ -1,52 +1,52 @@
 import { useEffect, useReducer, createContext } from "react";
 
-
 const initialState = {
-    user: null,
-
+  user: null,
 };
 
 const rootReducer = (state, action) => {
-    switch (action. type) {
-        case "LOGIN":
-            return {...state, user: action.payload};
-            case "LOGOUT":
-                return {...state, user: null};
-                default:
-                    return state;
-    }
-
+  switch (action.type) {
+    case "LOGIN":
+      return { ...state, user: action.payload };
+    case "LOGOUT":
+      return { ...state, user: null };
+    case "REGISTER":
+      return { ...state, user: action.payload };
+    default:
+      return state;
+  }
 };
-
 
 const AuthContext = createContext();
 
+const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(rootReducer, initialState);
 
+  useEffect(() => {
+    const user = window.localStorage.getItem("user");
+    if (user) {
+      dispatch({
+        type: "LOGIN",
+        payload: JSON.parse(user),
+      });
+    }
+    dispatch({
+      type: "REGISTER",
+      payload: JSON.stringify(window.localStorage.getItem("user")),
+    });
+  }, []);
 
-const AuthProvider = ({children}) => {
-    const [state, dispatch] = useReducer(rootReducer, initialState);
+  const logout = () => {
+    dispatch({ type: "LOGOUT" });
+    window.localStorage.removeItem("user");
+  };
 
-
-    useEffect(() => { //har uppdaterat detta 
-        const user = window.localStorage.getItem("user");
-        if (user) {
-            dispatch({
-                type: "LOGIN",
-                payload: JSON.parse(user)
-            });
-        }
-    }, []) //uppdaterat hit
-
-    const logout = () => {
-        dispatch({ type: "LOGOUT"});
-        window.localStorage.removeItem("user");
-    };
-
-    return(
-        <AuthContext.Provider value={{state, dispatch, logout}}>
-            {children}
-        </AuthContext.Provider>
-    )
+  return (
+    <AuthContext.Provider value={{ state, dispatch, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
-export {AuthContext, AuthProvider};
+export { AuthContext, AuthProvider };
+Collapse;
