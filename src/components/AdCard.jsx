b1669754b3f1bb5827dd5ddd773028e66d68db20
Fazilet1820 <senolfazilet1820@gmail.com>
2024-05-21 09:00:57 +0200

    cards design edited

--- src/components/AdCard.jsx
+++ src/components/AdCard.jsx
@@ -3,6 +3,65 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
 
 export default function AdCard(props) {
+  const cardStyle = {
+    width: "90%", // Yüzdelik değer kullanarak genişliği ayarlayın
+    maxWidth: "300px", // Maksimum genişliği belirleyin
+    margin: "10px",
+  };
+
+  const imgContainerStyle = {
+    width: "100%",
+    height: "200px", // Resim konteyneri için sabit yükseklik
+    overflow: "hidden", // Taşan kısımları gizle
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  const imgStyle = {
+    width: "auto",
+    height: "100%",
+    objectFit: "cover", // Resmi konteynerin boyutuna sığdır ve kırp
+  };
+
+  const listItemStyle = {
+    fontSize: "14px",
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-sm mb-3">
+          <div className="card" style={cardStyle}>
+            <div style={imgContainerStyle}>
+              <img src={props.imgUrl} alt="product image" style={imgStyle} />
+            </div>
+            <ul className="list-group list-group-flush">
+              <li
+                className="list-group-item d-flex justify-content-between"
+                style={listItemStyle}
+              >
+                {props.price}
+                <FontAwesomeIcon
+                  icon={faHeart}
+                  style={{ cursor: "pointer" }}
+                />{" "}
+              </li>
+              <li className="list-group-item" style={listItemStyle}>
+                {props.size}
+              </li>
+              <li className="list-group-item" style={listItemStyle}>
+                {props.category}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+/*export default function AdCard(props) {
   return (
     <div className="container">
       <div className="row">
@@ -29,4 +88,4 @@ export default function AdCard(props) {
       </div>
     </div>
   );
-}
+}*/
